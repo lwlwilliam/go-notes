724944fd81850bee977c71ae296f624433c99fe6
lwlwilliam <1053317536@qq.com>
2018-04-09 09:43:34 +0800

    go in action unit5: public and private struct

--- GoInAction/05 Go's type system/listing71/entities/entities.go
+++ GoInAction/05 Go's type system/listing71/entities/entities.go
@@ -1,1 +1,8 @@
+// entities 包包含系统中与人有关的类型
 package entities
+
+// User 在程序中定义一个用户类型
+type User struct {
+	Name string
+	email string  // 未公开字段
+}
--- GoInAction/05 Go's type system/listing71/listing71.go
+++ GoInAction/05 Go's type system/listing71/listing71.go
@@ -1,1 +1,19 @@
+// 这个示例程序展示公开的结构类型中未公开的字段无法直接访问
 package main
+
+import (
+	"fmt"
+	"./entities"
+)
+
+// main 是应用程序的入口 
+
+func main() {
+	// 创建 entities 包中的 User 类型的值
+	u := entities.User{
+		Name: "Bill", 
+		email: "bill@email.com",  // 结构字面量中结构 entities.User 的字段 email 未知，因此编译错误
+	}
+
+	fmt.Printf("User: %v\n", u)
+}
