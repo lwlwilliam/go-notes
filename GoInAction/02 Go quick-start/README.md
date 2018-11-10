f69673e1e6d37e2b44021bfd88f66bee1b3819c1
William-A <1053317536@qq.com>
2018-11-10 12:17:11 +0800

    clear up the hierarchy

--- GoInAction/02 Go quick-start/README.md
+++
@@ -1,7 +0,0 @@
-# 快速开始一个 Go 程序
-
-[sample](../code/chapter02/sample)
-
-## 程序架构
-
-
--- GoInAction/05 Go's type system/listing71/entities/entities.go
+++
@@ -1,8 +0,0 @@
-// entities 包包含系统中与人有关的类型
-package entities
-
-// User 在程序中定义一个用户类型
-type User struct {
-	Name string
-	email string  // 未公开字段
-}
