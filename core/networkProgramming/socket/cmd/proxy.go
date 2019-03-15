0cae90c96e1eff918838064e4b8069ddb1be05c7
lwlwilliam <1053317536@qq.com>
2019-03-15 17:06:32 +0800

    proxy log

--- core/networkProgramming/socket/cmd/proxy.go
+++ core/networkProgramming/socket/cmd/proxy.go
@@ -11,46 +11,49 @@ import (
 )
 
 func main() {
-	// 监听 tcp 8080 端口
-	socket, err := net.Listen("tcp", ":8888")
+	// 监听 tcp 60000 端口
+	socket, err := net.Listen("tcp", ":60000")
 	fatalError(err)
 
 	// 接收客户端请求
 	for {
-		client, err := socket.Accept()
+		request, err := socket.Accept()
 		fatalError(err)
 
 		// 处理请求
-		go requestHandler(client)
+		go requestHandler(request)
 	}
 }
 
-func requestHandler(client net.Conn) {
-	if client == nil {
+func requestHandler(request net.Conn) {
+	if request == nil {
 		return
 	}
 
-	defer client.Close()
+	defer request.Close()
 
+	// 获取请求内容
 	var buf [1024]byte
-	n, err := client.Read(buf[:])
+	n, err := request.Read(buf[:])
 	if err != nil {
-		log.Println(err)
+		log.Println("Read request:", err)
 		return
 	}
 
 	var method, host, address string
+
+	// 获取请求方法和主机
 	fmt.Sscanf(string(buf[:bytes.IndexByte(buf[:], '\n')]), "%s%s", &method, &host)
 	hostPortURL, err := url.Parse(host)
 	if err != nil {
-		log.Println(err)
+		log.Println("Parse host:", err)
 		return
 	}
 
+	// 确定端口
 	if hostPortURL.Opaque == "443" {
 		address = hostPortURL.Scheme + ":443"
 	} else {
-		// host 不带端口，默认 80
 		if strings.Index(hostPortURL.Host, ":") == -1 {
 			address = hostPortURL.Host + ":80"
 		} else {
@@ -58,25 +61,30 @@ func requestHandler(client net.Conn) {
 		}
 	}
 
+	// 到目标服务器的 socket
 	server, err := net.Dial("tcp", address)
 	if err != nil {
-		log.Println(err)
+		log.Println("Create socket to target server:", err)
 		return
 	}
 
+	// 记录请求 socket
+	log.Println("Attempt to request:", address)
+
+	// 创建隧道
 	if method == "CONNECT" {
-		fmt.Fprintf(client, "HTTP/1.1 200 Connection established\r\n\r\n")
+		fmt.Fprintf(request, "HTTP/1.1 200 Connection established\r\n\r\n")
 	} else {
 		server.Write(buf[:n])
 	}
 
 	// 进行转发
-	go io.Copy(server, client)
-	io.Copy(client, server)
+	go io.Copy(server, request)
+	io.Copy(request, server)
 }
 
 func fatalError(err error) {
 	if err != nil {
-		log.Panic(err)
+		log.Panic("Panic", err)
 	}
 }
