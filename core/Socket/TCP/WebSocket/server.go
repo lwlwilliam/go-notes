7d27aba173406c78e25d8e78c6280f44c74792f2
lwlwilliam <lwlwilliam.a@gmail.com>
2019-04-20 00:08:21 +0800

    websocket test

--- core/Socket/TCP/WebSocket/server.go
+++ core/Socket/TCP/WebSocket/server.go
@@ -5,35 +5,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Echo(ws *websocket.Conn) {
 	var err error
 
 	for {
-		var reply string
-
-		if err = websocket.Message.Receive(ws, &reply); err != nil {
-			fmt.Println("Can't receive")
-			break
-		}
-
-		fmt.Println("Received back from client: " + reply)
-
-		msg := "Received: " + reply
+		//var reply string
+		//
+		//if err = websocket.Message.Receive(ws, &reply); err != nil {
+		//	fmt.Println("Can't receive")
+		//	break
+		//}
+		//
+		//fmt.Println("Received back from client: " + reply)
+		//
+		//msg := "Received: " + reply
+		msg := "Hello world"
 		fmt.Println("Sending to client: " + msg)
 
 		if err = websocket.Message.Send(ws, msg); err != nil {
 			fmt.Println("Can't send")
 			break
 		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func main() {
 	http.Handle("/", websocket.Handler(Echo))
 
-	if err := http.ListenAndServe(":1234", nil); err != nil {
+	if err := http.ListenAndServe("localhost:1234", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
