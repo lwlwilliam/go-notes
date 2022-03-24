9560f5cc2de7f1ff53771bb49f857e4f4b45a72d
lwlwilliam <lwlwilliam@foxmail.com>
2022-03-24 17:25:57 +0800

    http server

--- socket/tcp/http/server.go
+++ socket/tcp/http/server.go
@@ -1,4 +1,5 @@
 // socket 实现的简单 http 服务器
+// 所有 TODO 已解决
 package main
 
 import (
@@ -7,12 +8,13 @@ import (
 )
 
 // HTTP 响应报文，行分隔符为 \r\n
-// TODO: 确定一下是否 200 报文要 Content-Length 头
-var content = []byte("HTTP/1.1 200 OK\r\nContent-type:text/plain\r\nContent-Length: 12\r\n\r\nHello world!")
+// TODO: 确定一下是否 200 报文要 Content-Length 头(DONE: 由于没有长度，客户端不知道什么时候应该结束，所以会一直读取数据，直到服务端关闭连接导致客户端出错，虽然客户端可以接收服务端发送的数据，但由于异常的关闭连接，所以一般会提示连接已被重置)
+// var content = []byte("HTTP/1.1 200 OK\r\nContent-type:text/plain\r\nContent-Length: 12\r\n\r\nHello world!")
+var content = []byte("HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\n")
 //var content = []byte("HTTP/1.1 302 Moved Temporarily\r\nLocation: http://example.com")
 
 func handleConn(conn *net.Conn) {
-	// TODO: 这里发现一定要把连接里的数据都读出来才能正常写入
+	// TODO: 这里发现一定要把连接里的数据都读出来才能正常写入(DONE: 不影响)
 	//var buf = make([]byte, 1) // buf 要确保所有数据都读出来了，最好循环读到 EOF
 	//_, err := (*conn).Read(buf)
 	//checkErr(err)
